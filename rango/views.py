e30a76db09555c38b13b49a8728130da2bf70c2d
Muiz Rusli <2783261r@student.gla.ac.uk>
2024-02-07 14:10:45 +0000

    Chapter 4 - post_test

--- rango/views.py
+++ rango/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage':'Cruncy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage':'Crunchy, creamy, cookie, candy, cupcake!'}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
@@ -12,5 +12,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'author':'This tutorial has been put together by muiz.'}
+    context_dict = {'author':'This tutorial has been put together by mooz.'}
     return render(request, 'rango/about.html', context=context_dict)
