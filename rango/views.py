db8c95954d16bb7f11b49585a88daa091319498f
Muiz Rusli <2783261r@student.gla.ac.uk>
2024-02-09 00:57:38 +0000

    Chapter 9 - pre_test

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 def index(request):
@@ -206,3 +206,8 @@ def user_login(request):
 @login_required
 def restricted(request):
     return HttpResponse("Since you're logged in, you can see this text.")
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
