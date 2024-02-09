8c5c70283dca1eb074abb7cdae3e339167b31604
Muiz Rusli <2783261r@student.gla.ac.uk>
2024-02-09 06:25:52 +0000

    Chapter 9 - pre_test

--- rango/views.py
+++ rango/views.py
@@ -59,6 +59,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -83,7 +84,9 @@ def add_category(request):
     # Render the form with error messages (if any)
     return render(request, 'rango/add_category.html', {'form':form})
 
+@login_required
 def add_page(request, category_name_slug):
+
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
@@ -205,7 +208,7 @@ def user_login(request):
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text.")
+    return render(request, 'rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
